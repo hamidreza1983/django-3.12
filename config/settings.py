f3faab1d0f0b6793e3a00f89acfd0ee68b14713a
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2025-12-26 03:28:31 +0100

    edit settings.py

--- config/settings.py
+++ config/settings.py
@@ -116,14 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/"/static"
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/"/media"
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "/static"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 STATICFILES_DIRS = [
-    BASE_DIR.joinpath("static"),
-    BASE_DIR.joinpath("media")
+    BASE_DIR / "static",
+    BASE_DIR / "media",
 ]
 
 # Default primary key field type
